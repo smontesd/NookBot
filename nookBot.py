c71fbdbc5ea08e05f3275fc79e527dcd63b18cb4
Stephen Montes De Oca <deocastephen@gmail.com>
2020-08-20 22:01:59 -0700

    CSV/villagers.csv modified

--- nookBot.py
+++ nookBot.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 
 # Constants
 BOT_PREFIX = '!'
-BOT_TOKEN = 'NzQyODQyMTM0MzA1OTY0MDYz.XzL_pg.vqnlcDW49O62icVoQbip7o7-jQg'
+BOT_TOKEN = 'BOT TOKEN GOES HERE'
 VILLAGERS_CSV = 'CSV/villagers.csv'
 FISH_CSV = 'CSV/fish.csv'
 SEACREATURES_CSV = 'CSV/seacreatures.csv'
